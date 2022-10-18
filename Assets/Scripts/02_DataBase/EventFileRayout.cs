3dfc9c3920e03f6418fad3a4d2f1004bb8843c61
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2022-10-18 10:48:54 +0900

    [데이터베이스 시스템] 사건 파일 CSV 기능 추가

--- Assets/Scripts/02_DataBase/EventFileRayout.cs
+++ Assets/Scripts/02_DataBase/EventFileRayout.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class EventFileRayout : MonoBehaviour
 {
 
     [Header("사건 정보")]
     [SerializeField] int _chapter;
-    [SerializeField] List<TextAsset> CSVList;
+    [SerializeField] string content;
+    [SerializeField] TextAsset csv;
 
 
     public int chapter
@@ -17,20 +20,29 @@ public class EventFileRayout : MonoBehaviour
         set { _chapter = value; }
     }
 
-
     [Header("오브젝트")]
     [SerializeField] Button exitbtn;
-    [SerializeField] Button eventFilebtn;
-    [SerializeField] Button diarybtn;
+    [SerializeField] Button figurebtn;
+    [SerializeField] Button provisobtn;
+    [SerializeField] TextMeshProUGUI contenttxt;
 
     [Header("레이아웃")]
     [SerializeField] GameObject eventFileRayout;
     [SerializeField] GameObject figureListRayout;
 
+    void Awake()
+    {
+        List<Dictionary<string, object>> infos = CSVReader.Read("CSVfiles/02_Database/Eventfiles/" + csv.name);
+        content = infos.Where(info => info["Chapter"].ToString() == chapter.ToString()).ToList()[0]["Content"].ToString();
+        contenttxt.text = content;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<Dictionary<string, object>> infos = CSVReader.Read("CSVfiles/02_Database/Eventfiles/" + csv.name);
+        content = infos.Where(info => info["Chapter"].ToString() == chapter.ToString()).ToList().First()["Content"].ToString();
+        contenttxt.text = content;
     }
 
     // Update is called once per frame
